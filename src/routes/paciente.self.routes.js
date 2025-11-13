11c68c469d29bf732fc14d2c60c303112020ab14
BrayanSena1 <brayan_moralesm@soy.sena.edu.co>
2025-11-12 23:06:22 -0500

    Update paciente.self.routes.js

--- src/routes/paciente.self.routes.js
+++ src/routes/paciente.self.routes.js
@@ -1,28 +1,31 @@
-// src/routes/paciente.self.routes.js
 import { Router } from "express";
 import { needAuth, needRole } from "../middleware/auth.js";
 import {
-  miPerfilPaciente,
+  miPaciente,
   actualizarMiPaciente,
   misCitas,
   cancelarMiCita,
   documentosDeMiCita,
-  miCertAfiliacion,
-  miCertHistorial,
+  certAfiliacionSelf,
+  certHistorialSelf,
+  crearMiCita
 } from "../controllers/pacienteSelf.controller.js";
 
 const r = Router();
+r.use(needAuth, needRole("paciente"));
 
-r.use(needAuth, needRole("paciente", "admin"));
-
-r.get("/", miPerfilPaciente);
+// Perfil
+r.get("/", miPaciente);
 r.put("/", actualizarMiPaciente);
 
+// Citas propias
 r.get("/citas", misCitas);
+r.post("/citas", crearMiCita);                 // 👈 nuevo
 r.patch("/citas/:id/cancelar", cancelarMiCita);
 r.get("/citas/:id/documentos", documentosDeMiCita);
 
-r.get("/certificados/afiliacion", miCertAfiliacion);
-r.get("/certificados/historial", miCertHistorial);
+// Certificados
+r.get("/certificados/afiliacion", certAfiliacionSelf);
+r.get("/certificados/historial", certHistorialSelf);
 
 export default r;
