d54b8d61085f3622b8db8dc760a3ed839e3714c2
BrayanSena1 <brayan_moralesm@soy.sena.edu.co>
2025-11-12 22:14:47 -0500

    Update auth.routes.js

--- src/routes/auth.routes.js
+++ src/routes/auth.routes.js
@@ -1,7 +1,11 @@
-import {Router} from "express";
-import {login,me} from "../controllers/auth.controller.js";
-import {needAuth} from "../middleware/auth.js";
+import { Router } from "express";
+import { login, me, register } from "../controllers/auth.controller.js";
+import { needAuth } from "../middleware/auth.js";
+
 const r = Router();
+
 r.post("/login", login);
+r.post("/register", register);
 r.get("/me", needAuth, me);
+
 export default r;
